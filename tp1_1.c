fadebd0993f7647fcbde101b265ed49825bcdf58
Jorge Gregorio Donaire Pereyra <jorgedonaire93@gmail.com>
2025-03-25 16:00:14 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,18 @@
+#include <stdio.h>
+
 int main()
 {
-    
-    printf("hola mundo");
+    int numero = 5;
+    int *Pnumero;
+
+    // *Pnumero = numero;
+    Pnumero = &numero;
+
+    printf("El contenido del puntero es: %d\n",*Pnumero);
+    printf("La dirección de memoria almacenada por el puntero es: %d\n",Pnumero);
+    printf("La dirección de memoria de la variable es: %d\n",&numero);
+    printf("La dirección de memoria del puntero es: %d\n",&Pnumero);
+    printf("El tamaño de memoria utilizado por esa variable es: %d\n",sizeof(int));
+
     return 0;
 }
